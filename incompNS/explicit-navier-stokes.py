eee5e2ce56dcc74549bd363baf2dd1abca68407f
Jesse <jchan985@gmail.com>
2013-12-20 15:08:51 -0600

    updating explicit ns

--- incompNS/explicit-navier-stokes.py
+++ incompNS/explicit-navier-stokes.py
@@ -31,10 +31,13 @@ from dolfin import *
 parameters["std_out_all_processes"] = False;
 
 # Load mesh from file
-mesh = Mesh("lshape.xml.gz")
+N = 16
+p_vel = 2
+mesh = UnitSquareMesh(N,N)
+hh = 1.0/(2.0*N) #circrumscribed circle defn
 
 # Define function spaces (P2-P1)
-V = VectorFunctionSpace(mesh, "CG", 2)
+V = VectorFunctionSpace(mesh, "CG", p_vel)
 Q = FunctionSpace(mesh, "CG", 1)
 
 # Define trial and test functions
@@ -44,38 +47,36 @@ v = TestFunction(V)
 q = TestFunction(Q)
 
 # Set parameter values
-dt = 0.01
-T = 3
+dt = 100*hh/(p_vel**2)
+print "dt = ", dt
+T = 100
 nu = 0.01
 
 # Define time-dependent pressure boundary condition
-p_in = Expression("sin(3.141592*t)", t=0.0)
-
+#p_in = Expression("sin(3.141592*t)", t=0.0)
+p_in = Expression("1.0")
+p_out = Expression("0.0")
 # Define boundary conditions
 noslip  = DirichletBC(V, (0, 0),
                       "on_boundary && \
-                       (x[0] < DOLFIN_EPS | x[1] < DOLFIN_EPS | \
-                       (x[0] > 0.5 - DOLFIN_EPS && x[1] > 0.5 - DOLFIN_EPS))")
+                       (x[0] < DOLFIN_EPS | x[0] > 1.0 - DOLFIN_EPS )")
 inflow  = DirichletBC(Q, p_in, "x[1] > 1.0 - DOLFIN_EPS")
-outflow = DirichletBC(Q, 0, "x[0] > 1.0 - DOLFIN_EPS")
+outflow = DirichletBC(Q, p_out, "x[1] < DOLFIN_EPS")
 bcu = [noslip]
 bcp = [inflow, outflow]
 
 # Create functions to store intermediate values
 u0 = Function(V)
 u1 = Function(V)
+u2 = Function(V)
 p1 = Function(Q)
 
 # Define coefficients
 k = Constant(dt)
 f = Constant((0, 0))
 
-# Explicit convective step
-
-
-# Tentative velocity step - change to 
-F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx + \
-     nu*inner(grad(u), grad(v))*dx - inner(f, v)*dx
+# Tentative velocity step
+F1 = (1/k)*inner(u - u0, v)*dx + inner(grad(u0)*u0, v)*dx - inner(f, v)*dx
 a1 = lhs(F1)
 L1 = rhs(F1)
 
@@ -87,10 +88,15 @@ L2 = -(1/k)*div(u1)*q*dx
 a3 = inner(u, v)*dx
 L3 = inner(u1, v)*dx - k*inner(grad(p1), v)*dx
 
+# Viscosity update
+a4 = inner(grad(u), grad(v))*dx + (1/(k*nu))*inner(u,v)*dx
+L4 = (1/(k*nu))*inner(u1, v)*dx
+
 # Assemble matrices
 A1 = assemble(a1)
 A2 = assemble(a2)
 A3 = assemble(a3)
+A4 = assemble(a4)
 
 # Use amg preconditioner if available
 prec = "amg" if has_krylov_solver_preconditioner("amg") else "default"
@@ -107,10 +113,15 @@ while t < T + DOLFIN_EPS:
     p_in.t = t
 
     # Compute tentative velocity step
-    begin("Computing tentative velocity")
+    begin("Convecting explicitly velocity")
     b1 = assemble(L1)
-    [bc.apply(A1, b1) for bc in bcu]
+    #[bc.apply(A1, b1) for bc in bcu]
     solve(A1, u1.vector(), b1, "gmres", "default")
+    #lumped = assemble(action(a1), coefficients=[Constant(1)])
+    #Uex = u1.vector()
+    #for i, bi in enumerate(b1):
+    #    Uex[i] = bi/lumped[i]
+    #u1.vector().set_local(Uex[V.dofmap().vertex_to_dof_map(mesh)])
     end()
 
     # Pressure correction
@@ -127,16 +138,22 @@ while t < T + DOLFIN_EPS:
     solve(A3, u1.vector(), b3, "gmres", "default")
     end()
 
+    begin("Computing viscous correction")
+    b4 = assemble(L4)
+    [bc.apply(A4, b4) for bc in bcu]
+    solve(A4, u2.vector(), b4, "gmres", "default")
+    end()
+
     # Plot solution
     plot(p1, title="Pressure", rescale=True)
-    plot(u1, title="Velocity", rescale=True)
+    plot(u2, title="Velocity", rescale=True)
 
     # Save to file
-    ufile << u1
+    ufile << u2
     pfile << p1
 
     # Move to next time step
-    u0.assign(u1)
+    u0.assign(u2)
     t += dt
     print "t =", t
 
